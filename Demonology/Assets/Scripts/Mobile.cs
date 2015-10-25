fec5db2991ba42a4d9ead65685b2d717ec69d427
Quinn Levine <Qlevine@uci.edu>
2015-10-25 13:54:24 -0700

    Added some colliders and prefabs so that the level one sprites have colliders and the lava forces the player to respawn when making contact.

---
+++ Demonology/Assets/Scripts/Mobile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mobile : MonoBehaviour {
+
+
+    public float speed = 1;
+    public float wallDist = 1.0f;
+    protected bool forwardDir;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Movement()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        {
+        forwardDir = !forwardDir;
+        }
+    }
+
+    //public void startDir()
+    //{
+
+    //}
+}
