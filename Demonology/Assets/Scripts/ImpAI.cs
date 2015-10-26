645f8dbaef763ca43a809f8a5ce212fb8c5aed95
Quinn Levine <Qlevine@uci.edu>
2015-10-25 17:05:22 -0700

    The imp now changes directions as soon as its raycast hits an object in front of it.  This still needs to be improved my changing the raycasts y to the position of the entities feet.  I added some new tags and layers as well.

--- Demonology/Assets/Scripts/ImpAI.cs
+++ Demonology/Assets/Scripts/ImpAI.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class ImpAI : Mobile {
 
 	// Use this for initialization
-	void Start () {
-	
+	public override void Start () {
+
+		Physics2D.IgnoreLayerCollision(10,9);
+		base.Start ();
 	}
-	
 	// Update is called once per frame
-	void Update () {
-        Movement();
-	
+
+	void OnDeath()
+	{
+		Physics2D.IgnoreLayerCollision (8, 9, false);
 	}
 }
--- Demonology/Assets/Scripts/Mobile.cs
+++ Demonology/Assets/Scripts/Mobile.cs
@@ -5,11 +5,35 @@ public class Mobile : MonoBehaviour {
 
 
     public float speed = 1;
-    public float wallDist = 1.0f;
-    protected int forwardDir = 1;
+    public float wallDist = .04f;
+	//Direction of the entity
+	protected Vector2 Dir;
 
-    public void Movement()
+
+	public virtual void Start()
+	{
+		//Change later for now default to right
+		Dir = Vector2.right;
+	}
+
+	public virtual void FixedUpdate()
+	{
+		Movement (new Ray2D(transform.position,Dir));
+	}
+    public virtual void Movement(Ray2D ry)
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime * forwardDir);
+		//Casts a ray in front of the object to see if there are any obstacles blocking the path
+		if (Physics2D.Raycast(ry.origin,ry.direction,wallDist,1<<8)) 
+		{
+			//Changes the direction the object faces to the opposite of its current direction
+			Dir = new Vector2(-Dir.x,Dir.y);
+
+
+		}
+
+		//Move forward
+		transform.Translate(Dir * speed * Time.deltaTime);
+		//Draws the Raycast so it is viewable in the editor
+		Debug.DrawRay (ry.origin, ry.direction,Color.red);
     }
 }
