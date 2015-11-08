fa38ee1b3e912e46c23e3bcde4a0b7aad1993627
dcockerham <dylantchaos@gmail.com>
2015-11-08 13:47:27 -0800

    Added crystal count display for the UI.

---
+++ Demonology/Assets/Scripts/UICrystalDisplay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UICrystalDisplay : MonoBehaviour {
+	
+	public Text crystalText;
+	public GameObject Player;
+	private int[] playerMats;
+
+	// Update is called once per frame
+	void Update () {
+		playerMats = Player.GetComponent<CharacterBehavior>().currentMats;
+		crystalText.text = playerMats[0].ToString();
+		print (playerMats [0]);
+	}
+}
