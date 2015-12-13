33d82ee8867fb281967f15ff32b0a7ca94660f07
Luke Lohden <lukasl@mac.com>
2015-12-13 11:55:54 -0800

    Added art and animations for the checkpoints! I tried to set a bool parameter for starting the animation but I don't want to spend all the time to understand how the checkpoint code works. I need someone to code in an "active" parameter for the animation to play.

--- Demonology/Assets/Scripts/Checkpoint.cs
+++ Demonology/Assets/Scripts/Checkpoint.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 public class Checkpoint : MonoBehaviour {
 
+	Animator animator;
+	public bool active = false; // False until player activates
+								// Used for enabling animation
+
 	// Use this for initialization
 	public GameObject player;
 	//set for crystals[]
@@ -11,17 +15,20 @@ public class Checkpoint : MonoBehaviour {
 	//If you respawn at a checkpoint, reinstantiate all the crystals in the set
 
 	void Start () {
-
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 
 
 	}
 
-	//void OnTriggerEnter2D(Collider2D other){
+	void OnTriggerEnter2D(Collider2D other){
 
-	//}
+		if (other.gameObject.tag == "Player") {
+			animator.SetBool ("active", true);
+		}
+	}
 }
