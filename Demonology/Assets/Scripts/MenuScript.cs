c67f8514dad6d17dc9e30bf6032039544b999056
Quinn Levine <Qlevine@uci.edu>
2015-12-16 21:37:23 -0800

    Main Menu buttons now work but with a small error that there is whitespace around the buttons that can be pressed as well.  To fix this problem we need to reduce the size of the image to just the size of the button itself.

---
+++ Demonology/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	
+	public Button startButton;
+	public Button quitButton;
+	// Use this for initialization
+	void Start () {
+		startButton = startButton.GetComponent<Button> ();
+		quitButton = quitButton.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartLevel()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+}
