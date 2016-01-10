0f935adf1ac5ee14a41e3119520e893fa03976f3
Shirby Wang <shirby.wang@gmail.com>
2016-01-10 14:35:31 -0800

    Testing attaching scripts

---
+++ Demonology/Assets/Scripts/StepSwitch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StepSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        MoveDown();
+        SomethingHappens();
+    }
+
+    public void MoveDown()
+    {
+        transform.Translate(Vector3.down, Space.World);
+    }
+
+    public void SomethingHappens()
+    {
+        //a door is opened?? idk how to do that yet
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
