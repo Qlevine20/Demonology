5af46ae674931a46ec0e1a4df4813ccce7128291
Tony Phan <phantj@uci.edu>
2016-01-18 13:41:50 -0800

    Experimenting with cloud sound effects (added Cloud Sounds script) & added more sounds for later

---
+++ Demonology/Assets/Scripts/CloudSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSound : MonoBehaviour {
+
+    public AudioClip[] lightningStrikes;
+
+
+	// Use this for initialization
+	void Start () {
+        AudioSource.PlayClipAtPoint(
+            lightningStrikes[Random.Range(0, lightningStrikes.Length)], transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
