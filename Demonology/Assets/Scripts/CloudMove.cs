7448f1e3103c1d2b56fc7f5013e2e3040f4f0817
Quinn Levine <Qlevine@uci.edu>
2016-01-18 01:15:04 -0800

    Added Clouds in the background.  Obviously they don't fit the art at the moment but I put in placeholder art for now.  This seems to be the best work around to animating the background.  This way sounds can come from every cloud as well.  Fixed wall jumping problem again because my changes don't seem to have been pushed correctly.

---
+++ Demonology/Assets/Scripts/CloudMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMove : MonoBehaviour {
+
+    private Animator cloudAnim;
+    public float speed;
+    private float count;
+    private float rand;
+	// Use this for initialization
+	void Start () {
+        count = 0;
+        rand = RandWait();
+        cloudAnim = GetComponent<Animator>();
+        cloudAnim.SetBool("Flash", true);
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //count += Time.deltaTime;
+        transform.Translate(-(speed * Time.deltaTime),0,0);
+        if (transform.position.x < CloudsSpawner.leftX) 
+        {
+            Destroy(gameObject);
+        }
+        //if (count > rand) 
+        //{
+        //    if (cloudAnim)
+        //    {
+        //        cloudAnim.SetBool("Flash", true);
+        //    }
+        //}
+
+	}
+
+
+
+    int RandWait()
+    {
+        return Random.Range(0, 8);
+    }
+}
---
+++ Demonology/Assets/Scripts/CloudsSpawner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsSpawner : MonoBehaviour {
+
+    public GameObject Cloud;
+    public GameObject Player;
+
+    public float left;
+    public float right;
+    public static float leftX;
+    public static float rightX;
+    private float counter = 0;
+    private bool CheckCreate;
+    private int wait_time;
+	// Use this for initialization
+    void Start() 
+    {
+        leftX = left;
+        rightX = right;
+        wait_time = RandWait();
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Player == null) 
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        counter += Time.deltaTime;
+        CheckCreate = RandomSpawnCloud(counter);
+        if (CheckCreate) 
+        {
+            wait_time = RandWait();
+            counter = 0;
+        }
+	    
+	}
+
+    bool RandomSpawnCloud(float counter) 
+    {
+        if (counter > wait_time) 
+        {
+            if (Player!=null)
+            {
+                Instantiate(Cloud, new Vector3(rightX, Player.transform.position.y + 8, transform.position.z), Quaternion.identity);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int RandWait() 
+    {
+        return Random.Range(0, 8);
+    }
+}
