d38700274f6479c7799e82a34357969693c06018
Shirby Wang <shirby.wang@gmail.com>
2016-02-28 14:31:00 -0800

    started power crystals and exit script

---
+++ Demonology/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitScript : MonoBehaviour
+{
+
+    public GameObject PowerCrystal;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (PowerCrystal == null)
+        {
+            PowerCrystal = GameObject.FindGameObjectWithTag("PowerCrystal");
+        }
+
+        //once no more crystals, activate the exit
+
+    }
+}
---
+++ Demonology/Assets/Scripts/PowerCrystal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerCrystal : EnemyBehavior
+{
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            OnDeath();
+        }
+
+    }
+}
