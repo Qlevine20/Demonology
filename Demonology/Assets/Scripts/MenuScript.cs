50b2b8a13f006c7b90f3afcb062322aef0aa340d
Quinn Levine <Qlevine@uci.edu>
2016-03-22 00:14:30 -0700

    Updated Start Menu, put in placeholders for controls screen, levels screen, and credits screen.

--- Demonology/Assets/Scripts/MenuScript.cs
+++ Demonology/Assets/Scripts/MenuScript.cs
@@ -6,16 +6,73 @@ public class MenuScript : MonoBehaviour {
 	
 	public Button startButton;
 	public Button quitButton;
+    public Button controlsButton;
+    public Button levelsButton;
+    public Button creditsButton;
+    public GameObject ControlsScreen;
+    public GameObject MainMenu;
+    public GameObject LevelsScreen;
+    public GameObject CreditsScreen;
 	// Use this for initialization
 	void Start () {
 		startButton = startButton.GetComponent<Button> ();
 		quitButton = quitButton.GetComponent<Button> ();
+        controlsButton = controlsButton.GetComponent<Button>();
+        levelsButton = levelsButton.GetComponent<Button>();
+        creditsButton = creditsButton.GetComponent<Button>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void BackToMenu() 
+    {
+        ControlsScreen.SetActive(false);
+        LevelsScreen.SetActive(false);
+        CreditsScreen.SetActive(false);
+        startButton.interactable = true;
+        quitButton.interactable = true;
+        controlsButton.interactable = true;
+        levelsButton.interactable = true;
+        creditsButton.interactable = true;
+
+    }
+
+    public void ControlsMenuButton() 
+    {
+        ControlsScreen.SetActive(true);
+        startButton.interactable = false;
+        quitButton.interactable = false;
+        controlsButton.interactable = false;
+        levelsButton.interactable = false;
+        creditsButton.interactable = false;
+
+
+    }
+
+    public void LevelsMenuButton() 
+    {
+        LevelsScreen.SetActive(true);
+        startButton.interactable = false;
+        quitButton.interactable = false;
+        controlsButton.interactable = false;
+        levelsButton.interactable = false;
+        creditsButton.interactable = false;
+
+    }
+
+    public void CreditsMenuButton() 
+    {
+        CreditsScreen.SetActive(true);
+        startButton.interactable = false;
+        quitButton.interactable = false;
+        controlsButton.interactable = false;
+        levelsButton.interactable = false;
+        creditsButton.interactable = false;
+    }
 
 	public void StartLevel()
 	{
