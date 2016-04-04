ac7f2f66fd154383ab3132bdf0260c38e38c6160
Quinn Levine <Qlevine@uci.edu>
2016-04-03 19:47:42 -0700

    Changed the color of the power crystals so they don't match the background color so much and added a particle effect to the power crystals when the player destroys them.

--- Demonology/Assets/Scripts/PowerCrystal.cs
+++ Demonology/Assets/Scripts/PowerCrystal.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class PowerCrystal : EnemyBehavior
 {
+    public GameObject PartEffect;
 	public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
+            Instantiate(PartEffect, transform.position,Quaternion.identity);
             OnDeath();
         }
 	}
