dd0c35154704e87d5452530fb731c3101941dd0d
dcockerham <dylantchaos@gmail.com>
2016-04-24 15:12:24 -0700

    Implemented the other half of the UI changes, and adjusted imps and crystals to use the new settings.

--- Demonology/Assets/Scripts/PowerCrystal.cs
+++ Demonology/Assets/Scripts/PowerCrystal.cs
@@ -3,7 +3,40 @@ using System.Collections;
 
 public class PowerCrystal : EnemyBehavior
 {
-    public GameObject PartEffect;
+	public int baubleNumber = 0;
+	public GameObject PartEffect;
+	public GameObject bauble;
+	Animator anim;
+
+	public override void Start()
+	{
+		base.Start ();
+		switch (baubleNumber) {
+		case 0:
+			bauble = GameObject.Find("Bauble (0)");
+			break;
+		case 1:
+			bauble = GameObject.Find("Bauble (1)");
+			break;
+		case 2:
+			bauble = GameObject.Find("Bauble (2)");
+			break;
+		case 3:
+			bauble = GameObject.Find("Bauble (3)");
+			break;
+		case 4:
+			bauble = GameObject.Find("Bauble (4)");
+			break;
+		case 5:
+			bauble = GameObject.Find("Bauble (5)");
+			break;
+		default:
+			bauble = GameObject.Find("Bauble (0)");
+			break;
+		}
+		bauble.GetComponent<Animator> ().SetInteger ("ActiveColor", 1);
+	}
+
 	public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
@@ -17,5 +50,17 @@ public class PowerCrystal : EnemyBehavior
 			Instantiate(PartEffect, transform.position,Quaternion.identity);
 			OnDeath();
 		}
+	}
+
+	public override void OnDeath ()
+	{
+		base.OnDeath ();
+		bauble.GetComponent<Animator> ().SetInteger ("ActiveColor", 2);
+	}
+
+	public override void OnRespawn ()
+	{
+		base.OnRespawn ();
+		bauble.GetComponent<Animator> ().SetInteger ("ActiveColor", 1);
 	}
 }
