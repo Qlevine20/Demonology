8b31300d271d29564ba86f4bb8d9ecb39506bf8c
Quinn Levine <Qlevine@uci.edu>
2016-04-27 18:58:41 -0700

    The eyes work for now but they only work on circular eyes.  A radius can be set to determine how far the eyes can move and so this radius can be set based on the size of the eyes.

--- Demonology/Assets/Scripts/EyeScript.cs
+++ Demonology/Assets/Scripts/EyeScript.cs
@@ -2,153 +2,52 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class EyeScript : MonoBehaviour {
+public class EyeScript : MonoBehaviour
+{
 
     public GameObject RightEye;
     public GameObject LeftEye;
     private Transform Player;
     private Transform LeftPupil;
     private Transform RightPupil;
-    private Vector3 LeftStart;
-    private Vector3 RightStart;
-    public float speed;
-    public float mult;
-    private List<Vector2> CollPointsLeft;
-    private List<Vector2> CollPointsRight;
-    private int LeftIndex;
-    private int RightIndex;
 
-	// Use this for initialization
-	void Start () {
+    public float speed;
+    public int radius;
+
+    public int divider;
+
+    // Use this for initialization
+    void Start()
+    {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
         LeftPupil = LeftEye.transform.FindChild("EyePupil");
         RightPupil = RightEye.transform.FindChild("EyePupil");
-        LeftStart = LeftPupil.position;
-        RightStart = RightPupil.position;
-        
-        
-        CollPointsLeft = new List<Vector2>(LeftEye.GetComponentInChildren<PolygonCollider2D>().points);
-        CollPointsRight = new List<Vector2>(RightEye.GetComponentInChildren<PolygonCollider2D>().points);
-        CollPointsLeft.Sort(delegate(Vector2 a, Vector2 b)
-            {
-                return (a.x).CompareTo(b.x);
-            });
-        CollPointsRight.Sort(delegate(Vector2 a, Vector2 b)
-        {
-            return (a.x).CompareTo(b.x);
-        });
+    }
 
-
-        foreach (Vector2 point in CollPointsLeft) 
-        {
-         //   Debug.Log(point);
-        }
-
-        LeftIndex = FindClosestIndexStart(CollPointsLeft);
-        RightIndex = FindClosestIndexStart(CollPointsRight);
-
-
-        Vector2 LeftChange = CollPointsLeft[LeftIndex];
-        Debug.Log(LeftChange);
-        Vector3 NewLeftPos = new Vector3(LeftChange.x * LeftEye.transform.localScale.x + LeftStart.x, LeftChange.y * LeftEye.transform.localScale.y + LeftStart.y, LeftPupil.transform.position.z);
-        LeftPupil.transform.position = Vector3.Lerp(LeftPupil.transform.position, NewLeftPos, speed * Time.deltaTime);
-
-
-
-        Vector2 RightChange = CollPointsRight[RightIndex];
-        Vector3 NewRightPos = new Vector3(RightChange.x * RightEye.transform.localScale.x + RightStart.x, RightChange.y * RightEye.transform.localScale.y + RightStart.y, RightPupil.transform.position.z);
-        RightPupil.transform.position = Vector3.Lerp(RightPupil.transform.position, NewRightPos, speed * Time.deltaTime);
-	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
+    // Update is called once per frame
+    void LateUpdate()
+    {
         if (Player != null)
         {
+            //Debug.DrawRay(LeftEye.transform.position, Player.transform.position - (transform.position - LeftEye.transform.position) - LeftEye.transform.position, Color.green);
+            //Debug.DrawRay(RightEye.transform.position, Player.transform.position - (transform.position - LeftEye.transform.position) - LeftEye.transform.position, Color.green);
 
-            Vector2 LeftChange = FindClosestPoint(CollPointsLeft, LeftIndex, true);
-            //Debug.Log(LeftChange);
 
-            Vector3 NewLeftPos = new Vector3(LeftChange.x * LeftEye.transform.localScale.x + LeftStart.x, LeftChange.y * LeftEye.transform.localScale.y + LeftStart.y, LeftPupil.transform.position.z);
+            Vector3 Diff = transform.position - LeftEye.transform.position;
 
-            //Debug.Log(NewLeftPos);
-            LeftPupil.transform.position = Vector3.Lerp(LeftPupil.transform.position, NewLeftPos, speed * Time.deltaTime);
+            Vector3 Lvector = new Vector3(LeftEye.transform.position.x + ((Player.transform.position.x + Diff.x) / divider), LeftEye.transform.position.y + ((Player.transform.position.y)), LeftPupil.transform.position.z);
+            Vector3 Rvector = new Vector3(RightEye.transform.position.x + ((Player.transform.position.x + Diff.x) / divider), RightEye.transform.position.y + ((Player.transform.position.y)), RightPupil.transform.position.z);
 
-            //Debug.Log(RightIndex);
-            Vector2 RightChange = FindClosestPoint(CollPointsRight, RightIndex, false);
 
-            Vector3 NewRightPos = new Vector3(RightChange.x * RightEye.transform.localScale.x + RightStart.x, RightChange.y * RightEye.transform.localScale.y + RightStart.y, RightPupil.transform.position.z);
-
-            RightPupil.transform.position = Vector3.Lerp(RightPupil.transform.position, NewRightPos, speed * Time.deltaTime);
+            if (Vector3.Distance(LeftEye.transform.position, Lvector) * .9f < radius)
+            {
+                LeftPupil.transform.position = Vector3.Lerp(LeftPupil.transform.position, Lvector, speed * Time.deltaTime);
+                RightPupil.transform.position = Vector3.Lerp(RightPupil.transform.position, Rvector, speed * Time.deltaTime);
+            }
         }
-        else 
+        else
         {
             Player = GameObject.FindGameObjectWithTag("Player").transform;
         }
-	}
-
-
-    //public float Offset(bool left)
-    //{
-    //    if (left)
-    //    {
-    //        return -(transform.position.x - Player.transform.position.x) / (transform.position.z / speed);
-    //    }
-    //    return -(transform.position.x - Player.transform.position.x) / (transform.position.z / speed);
-    //}
-
-    public Vector2 FindClosestPoint(List<Vector2> CollPoints, int indexLoc, bool left) 
-    {
-
-        Vector2 minVec = CollPoints[indexLoc];
-
-        if (indexLoc + 1 < CollPoints.Count && Vector2.Distance(new Vector2(Player.transform.position.x, Player.transform.position.y), new Vector2(CollPoints[indexLoc + 1].x + LeftStart.x, CollPoints[indexLoc + 1].y + LeftStart.y)) < Vector2.Distance(Player.transform.position, new Vector2(minVec.x + LeftStart.x, minVec.y + LeftStart.y)))
-        {
-
-            if (left)
-            {
-                LeftIndex++;
-            }
-            else
-            {
-                RightIndex++;
-            }
-            //Debug.Log("Change");
-            return CollPoints[indexLoc + 1];
-        }
-        if (indexLoc - 1 >= 0 && Vector2.Distance(new Vector2(Player.transform.position.x/mult,Player.transform.position.y), new Vector2(CollPoints[indexLoc - 1].x + LeftStart.x, CollPoints[indexLoc - 1].y + LeftStart.y)) < Vector2.Distance(Player.transform.position/mult, new Vector2(minVec.x + LeftStart.x, minVec.y + LeftStart.y)))
-        {
-
-            if (left)
-            {
-                LeftIndex--;
-            }
-            else
-            {
-                RightIndex--;
-            }
-            //Debug.Log("Change");
-            return CollPoints[indexLoc - 1];
-        }
-        return minVec;
-
-
-    }
-
-
-    public int FindClosestIndexStart(List<Vector2> CollPoints) 
-    {
-        int minIndex = 0;
-        int count = 0;
-        foreach (Vector2 point in CollPoints)
-        {
-            //Debug.Log(point);
-            if (Vector2.Distance(new Vector2(Player.transform.position.x, Player.transform.position.y), new Vector2((point.x) * mult + LeftStart.x, point.y * mult + LeftStart.y)) < Vector2.Distance(new Vector2(Player.transform.position.x, Player.transform.position.y), new Vector2(CollPoints[minIndex].x * mult + LeftStart.x, CollPoints[minIndex].y * mult + LeftStart.y)))
-            {
-                minIndex = count;
-            }
-            count++;
-        }
-        //Debug.Log(minIndex);
-        return minIndex;
     }
 }
