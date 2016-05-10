d798fd641d6a77ddffdde92322059058363f6b1e
Quinn Levine <Qlevine@uci.edu>
2016-05-09 22:53:06 -0700

    The levels screen now resizes and relocates based on the screen size.  The levels earned by the player is now saved to a file as well and the player cannot use the levels screen to go to levels they have not been to yet.  I added the rest of the levels to the build settings so that I all the buttons work correctly.

---
+++ Demonology/Assets/LevelReScale.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelReScale : MonoBehaviour {
+
+    public GameObject[] Buttons;
+    public int columnSize = 5;
+    public int rowSize = 3;
+	// Use this for initialization
+	void Start () {
+        float currR = rowSize-1;
+        float currC = 0;
+        int levelNum = 0;
+        foreach (GameObject b in Buttons) 
+        {
+            levelNum++;
+            RectTransform buttonRect = b.GetComponent<RectTransform>();
+            //buttonRect.localScale = new Vector3(buttonRect.localScale.x * Screen.width / buttonRect.rect.width/Buttons.Length,buttonRect.localScale.y * Screen.height / buttonRect.rect.height/Buttons.Length, 1);
+            if (currC > columnSize-1) 
+            {
+                currC = 0;
+                currR--;
+            }
+            if (currC < (columnSize)/2)
+            {
+                buttonRect.anchoredPosition = new Vector2(-buttonRect.rect.width * (((columnSize)/2) - currC), buttonRect.rect.height * currR);
+            }
+            else if (currC == (columnSize-1) / 2.0f)
+            {
+                
+                buttonRect.anchoredPosition = new Vector2(0, buttonRect.rect.height * currR);
+            }
+            else
+            {
+                
+                buttonRect.anchoredPosition = new Vector2(buttonRect.rect.width * (currC - (columnSize/2)), buttonRect.rect.height * currR);
+            }
+            if (PersistantBehavoir.levelsEarned < levelNum) 
+            {
+                b.SetActive(false);
+            }
+
+
+            currC++;
+            
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Demonology/Assets/PersistantBehavoir.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class PersistantBehavoir : MonoBehaviour {
+
+    public static int levelsEarned = 0;
+	// Use this for initialization
+	void Start () {
+        if (levelsEarned < Application.loadedLevel && Application.loadedLevelName != "LoadingScreen") 
+        {
+            
+            levelsEarned = Application.loadedLevel;
+            Save();
+        }
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Save() 
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/levelInfo.dat");
+
+        PlayerData data = new PlayerData();
+        data.levelsEarned = levelsEarned;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    void Load() 
+    {
+        if (File.Exists(Application.persistentDataPath + "/levelInfo.dat")) 
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/levelInfo.dat",FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+            levelsEarned = data.levelsEarned;
+        }
+    }
+
+    [System.Serializable]
+    class PlayerData     
+    {
+        public int levelsEarned;
+    }
+
+}
