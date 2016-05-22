f5b2104ce617da84499dcd45a8d12efd69c5d9b5
Quinn Levine <Qlevine@uci.edu>
2016-05-22 15:31:37 -0700

    Worked on end scene, added text and a devil sprite.  Also included the sounds for the end scene they just need to be timed through script.

---
+++ Demonology/Assets/Devil.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Devil : MonoBehaviour {
+
+
+    public float WalkingSpeed = 5;
+    public float TimeWalking = 5;
+    private float counter = 0;
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (TimeWalking > counter)
+        {
+            counter+=Time.deltaTime;
+            transform.Translate(new Vector3(-(WalkingSpeed * Time.deltaTime), 0.0f, 0.0f));
+           
+        }
+        else if (anim.GetBool("Walking") == true) 
+        {
+            anim.SetBool("Walking",false);
+        }
+        
+	}
+}
---
+++ Demonology/Assets/TextBoxScripts.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBoxScripts : MonoBehaviour {
+    public float StartOffset = 5;
+    public float DisplayTime = 5;
+    public float NumTextBoxes;
+    private float counter = 0;
+    private int Tbox = 0;
+    private bool Started = false;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Tbox < NumTextBoxes - 1)
+        {
+            counter += Time.deltaTime;
+            if (StartOffset < counter && Started == false) 
+            {
+                Started = true;
+                counter = 0;
+                transform.GetChild(Tbox).gameObject.SetActive(true);
+            }
+            else if (DisplayTime < counter)
+            {
+                counter = 0;
+                transform.GetChild(Tbox).gameObject.SetActive(false);
+                Tbox++;
+                transform.GetChild(Tbox).gameObject.SetActive(true);
+            }
+        }
+    }
+}
