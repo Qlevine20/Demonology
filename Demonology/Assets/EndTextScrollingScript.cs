04a3089f5a9b3de9c7a84646be8af91044a95bc6
Quinn Levine <Qlevine@uci.edu>
2016-05-30 13:00:18 -0700

    One of the scripts got removed from the animator but I attached it back to the respawn state and now all the deaths work again. Not sure how this happened but it is fixed now.  Also pushing the end story scene with scrolling text.

---
+++ Demonology/Assets/EndTextScrollingScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTextScrollingScript : MonoBehaviour {
+
+    public float speed;
+    public Transform EndScroll;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = new Vector3(0, speed * Time.deltaTime, 0);
+	    transform.Translate(move);
+        EndScroll.transform.Translate(move);
+        if (EndScroll.transform.position.y > 0) 
+        {
+            Application.LoadLevel(0);
+        }
+	}
+}
